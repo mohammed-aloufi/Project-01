1770aad61c5e4deb7392c11a422b3bc813cf6b3b
Walaa Alshaikh <walaalshaikh94@gmail.com>
2022-01-12 19:50:38 +0300

    finished the splash, adding the dependencies

--- app/src/main/java/com/example/scrolly/main/Splash.kt
+++ app/src/main/java/com/example/scrolly/main/Splash.kt
@@ -1,12 +1,52 @@
 package com.example.scrolly.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.example.scrolly.R
+import com.example.scrolly.databinding.ActivitySplashBinding
 
 class Splash : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-    }
-}
+
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+              //
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+              //
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+
+                val intent = Intent(this@Splash,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+               //
+            }
+        })
+}}
